d66e36669f42ef96381f25231bf0a02d96d2c36b
stefaan himpe <stefaan.himpe@gmail.com>
2021-09-26 22:34:28 +0200

    some reorganization of the code: extract some things into functions and move testset definitions to top of file

--- main.py
+++ main.py
@@ -17,7 +17,50 @@ from mido import Message
 
 TimelineEntry = namedtuple("TimelineEntry", "start stop note velocity")
 Event = namedtuple("Event", "type time note velocity")
-TestSetEntry = namedtuple("TestSetEntry", "filename velocity_threshold min_duration max_note min_note min_amplitude_db transposition")
+TestSetEntry = namedtuple("TestSetEntry",
+                          "filename velocity_threshold min_duration max_note min_note min_amplitude_db transposition")
+
+test_sets = {
+    # entry 0 is the worst :)
+    0: TestSetEntry(filename='inputs/hello-long-filtered.wav',  # hello, hello, hello
+                    min_note=0,
+                    max_note=100,
+                    transposition=0,
+                    min_amplitude_db=-180,
+                    velocity_threshold=101,
+                    min_duration=0.02),
+    1: TestSetEntry(filename='inputs/scared.wav',  # i don't wanna go, i'm scared
+                    min_note=0,
+                    max_note=127,
+                    transposition=0,
+                    min_amplitude_db=-153,
+                    velocity_threshold=100,
+                    min_duration=0.02),
+    2: TestSetEntry(filename="inputs/peace.wav",
+                    # once this was a peaceful place, until one day it all went out of control
+                    min_note=0,
+                    max_note=127,
+                    transposition=0,
+                    min_amplitude_db=-160,
+                    velocity_threshold=93,
+                    min_duration=0.02),
+    3: TestSetEntry(filename="inputs/ohmy.wav",  # oh my
+                    min_note=0,
+                    max_note=127,
+                    transposition=0,
+                    min_amplitude_db=-140,
+                    velocity_threshold=100,
+                    min_duration=0.02),
+    4: TestSetEntry(filename="inputs/greatestshow.wav",
+                    # welcome ladies and gentlemen and welcome to the greatest show on earth
+                    min_note=0,
+                    max_note=127,
+                    transposition=-3,
+                    min_amplitude_db=-190,
+                    velocity_threshold=100,
+                    min_duration=0.02),
+}
+test_id = 1
 
 
 def round_half_up(n, decimals=0):
@@ -78,129 +121,9 @@ def remove_short_events(timeline, min_time):
 
 
 def print_stats(midinotes):
-    #h = np.histogram(midinotes, 128, (0, 127))
-    #print(h[0])
+    # h = np.histogram(midinotes, 128, (0, 127))
+    # print(h[0])
     pass
-
-def main():
-    test_sets = {
-        # entry 0 is the worst :)
-        0: TestSetEntry(filename='inputs/hello-long-filtered.wav',
-                        min_note=0,
-                        max_note=100,
-                        transposition=0,
-                        min_amplitude_db=-180,
-                        velocity_threshold=101,
-                        min_duration=0.02),
-        1: TestSetEntry(filename='inputs/scared.wav',
-                        min_note=0,
-                        max_note=127,
-                        transposition=0,
-                        min_amplitude_db=-153,
-                        velocity_threshold=100,
-                        min_duration=0.02),
-        2: TestSetEntry(filename="inputs/peace.wav",
-                        min_note=0,
-                        max_note=127,
-                        transposition=0,
-                        min_amplitude_db=-160,
-                        velocity_threshold=93,
-                        min_duration=0.02),
-        3: TestSetEntry(filename="inputs/ohmy.wav",
-                        min_note=0,
-                        max_note=127,
-                        transposition=0,
-                        min_amplitude_db=-140,
-                        velocity_threshold=100,
-                        min_duration=0.02),
-        4: TestSetEntry(filename="inputs/greatestshow.wav",
-                        min_note=0,
-                        max_note=127,
-                        transposition=-3,
-                        min_amplitude_db=-190,
-                        velocity_threshold=100,
-                        min_duration=0.02),
-    }
-    test_id = 3
-    own_path = pathlib.Path(sys.argv[0]).parent
-    fs, audio = wavfile.read(own_path.joinpath(test_sets[test_id].filename))
-
-    if audio.ndim == 1:
-        mono = audio / (2 ** 15)
-    else:
-        mono = audio.sum(axis=1) / audio.shape[1] / (2 ** 15)
-
-    # hfreq, hmag, hphase = analyse_audio_sms_tools(fs, mono, own_path)
-    hfreq, hmag, hphase = analyse_audio_stft(fs, mono, own_path)
-
-    if len(hfreq) > 0:
-        score = []
-        no_of_lines = len(hfreq)
-        duration = audio.shape[0] / fs
-        time_step = duration / no_of_lines
-        max_amp = -1e10
-        min_amp = 1e10
-        for m in hmag:
-            relevant_amps = [el for el in m if el > test_sets[test_id].min_amplitude_db]
-            max_amp = max([max_amp, max(relevant_amps)])
-            min_amp = min([min_amp, min(relevant_amps)])
-        print(f"{max_amp = }, {min_amp = }")
-
-        for f, m in zip(hfreq, hmag):
-            midinotes = [int(round_half_up(cpsmidi(freq))) + test_sets[test_id].transposition if freq > 0 else 0 for freq in f]
-            midinotes_filtered = [e if test_sets[test_id].min_note <= e <= test_sets[test_id].max_note else 0 for e in
-                                  midinotes]
-            print_stats(midinotes_filtered)
-            amps = [el if el >= test_sets[test_id].min_amplitude_db else min_amp for el in m]
-            mapped_amps = [Mapping.linlin(a, min_amp, max_amp, 0, 127) for a in amps]
-            rescaled_amps = [int(round_half_up(el)) for el in mapped_amps]
-            score.append(
-                [(note, amp) for note, amp in zip(midinotes_filtered, rescaled_amps) if note != 0 and amp != 0])
-
-        timeline = distill_timeline(time_step, score, test_sets[test_id].velocity_threshold)
-        filtered_timeline = remove_short_events(timeline, test_sets[test_id].min_duration)
-        event_list = distill_event_list(filtered_timeline)
-
-        outport = mido.open_output('INTEGRA-7:INTEGRA-7 MIDI 1 28:0')
-
-        for i in range(100):
-            previous_time = 0
-            for event in event_list:
-                new_time = event.time
-                if new_time == previous_time:
-                    outport.send(Message(event.type, channel=0, note=event.note, velocity=event.velocity))
-                else:
-                    delta = new_time - previous_time
-                    previous_time = new_time
-                    time.sleep(delta)
-
-            time.sleep(1.0)
-            outport.reset()
-
-
-def analyse_audio_sms_tools(fs, mono, own_path):
-    analysis_window_size = 1201
-    window = 'blackman'
-    fft_size = 2048
-    w = get_window(window, analysis_window_size)
-    spectral_peak_threshold_db = -90
-    max_harmonics = 60
-    min_f0 = 130
-    max_f0 = 1000
-    error_threshold_f0_detection = 5
-    harm_dev_slope = 0.1
-    min_sine_dur = 0.02
-    hop_size = 128
-    check_model = harmonicModel.harmonicModel(x=mono, fs=fs, w=w, N=fft_size, t=spectral_peak_threshold_db,
-                                              nH=max_harmonics, minf0=min_f0, maxf0=max_f0,
-                                              f0et=error_threshold_f0_detection)
-    wavfile.write(own_path.joinpath("outputs/check_harmonic.wav"), fs, check_model)
-    hfreq, hmag, hphase = harmonicModel.harmonicModelAnal(x=mono, fs=fs, w=w, N=fft_size, H=hop_size,
-                                                          t=spectral_peak_threshold_db,
-                                                          nH=max_harmonics, minf0=min_f0, maxf0=max_f0,
-                                                          f0et=error_threshold_f0_detection,
-                                                          harmDevSlope=harm_dev_slope, minSineDur=min_sine_dur)
-    return hfreq, hmag, hphase
 
 
 def make_odd(num):
@@ -223,6 +146,74 @@ def analyse_audio_stft(fs, mono, own_path):
     hfreq = [[i * spacing for i in range(fft_size // 2)] for _ in range(len(hmag))]
     print(f"frequency bin spacing = {spacing} Hz")
     return hfreq, hmag, hphase
+
+
+def convert_freq_mag_to_event_list(duration, hfreq, hmag):
+    global test_sets
+    global test_id
+    if len(hfreq) > 0:
+        score = []
+        no_of_lines = len(hfreq)
+        time_step = duration / no_of_lines
+        max_amp = -1e10
+        min_amp = 1e10
+        for m in hmag:
+            relevant_amps = [el for el in m if el > test_sets[test_id].min_amplitude_db]
+            max_amp = max([max_amp, max(relevant_amps)])
+            min_amp = min([min_amp, min(relevant_amps)])
+        print(f"{max_amp = }, {min_amp = }")
+
+        for f, m in zip(hfreq, hmag):
+            midinotes = [int(round_half_up(cpsmidi(freq))) + test_sets[test_id].transposition if freq > 0 else 0 for
+                         freq in f]
+            midinotes_filtered = [e if test_sets[test_id].min_note <= e <= test_sets[test_id].max_note else 0 for e in
+                                  midinotes]
+            print_stats(midinotes_filtered)
+            amps = [el if el >= test_sets[test_id].min_amplitude_db else min_amp for el in m]
+            mapped_amps = [Mapping.linlin(a, min_amp, max_amp, 0, 127) for a in amps]
+            rescaled_amps = [int(round_half_up(el)) for el in mapped_amps]
+            score.append(
+                [(note, amp) for note, amp in zip(midinotes_filtered, rescaled_amps) if note != 0 and amp != 0])
+
+        timeline = distill_timeline(time_step, score, test_sets[test_id].velocity_threshold)
+        filtered_timeline = remove_short_events(timeline, test_sets[test_id].min_duration)
+        event_list = distill_event_list(filtered_timeline)
+        return event_list
+
+    return []
+
+
+def perform_event_list(event_list):
+    if event_list:
+        outport = mido.open_output('INTEGRA-7:INTEGRA-7 MIDI 1 28:0')
+
+        for i in range(100):
+            previous_time = 0
+            for event in event_list:
+                new_time = event.time
+                if new_time == previous_time:
+                    outport.send(Message(event.type, channel=0, note=event.note, velocity=event.velocity))
+                else:
+                    delta = new_time - previous_time
+                    previous_time = new_time
+                    time.sleep(delta)
+
+            time.sleep(1.0)
+            outport.reset()
+
+
+def main():
+    own_path = pathlib.Path(sys.argv[0]).parent
+    fs, audio = wavfile.read(own_path.joinpath(test_sets[test_id].filename))
+
+    if audio.ndim == 1:
+        mono = audio / (2 ** 15)
+    else:
+        mono = audio.sum(axis=1) / audio.shape[1] / (2 ** 15)
+
+    hfreq, hmag, hphase = analyse_audio_stft(fs, mono, own_path)
+    event_list = convert_freq_mag_to_event_list(audio.shape[0] / fs, hfreq, hmag)
+    perform_event_list(event_list)
 
 
 if __name__ == '__main__':
